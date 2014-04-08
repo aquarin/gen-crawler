80910bab3f5c317ea9435254e4d60d35e0919007
aquarin <shuishiy@shuishiy-macbookair.roam.corp.google.com>
2014-04-08 09:39:10 -0700

    Add abstract classes

---
+++ gen-crawler/src/com/aquarin/crawler/AbstractDocument.java
@@ -0,0 +1,5 @@
+package com.aquarin.crawler;
+
+public abstract class AbstractDocument {
+
+}
---
+++ gen-crawler/src/com/aquarin/crawler/AbstractDocumentFetcher.java
@@ -0,0 +1,5 @@
+package com.aquarin.crawler;
+
+public abstract class AbstractDocumentFetcher {
+	abstract AbstractDocument fetchDocument(AbstractUrl url);
+}
