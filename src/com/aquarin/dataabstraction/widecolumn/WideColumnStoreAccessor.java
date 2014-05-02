66a26815a5dd42e112458830e7f60d1362f7698e
aquarin <accounts.yang@gmail.com>
2014-05-02 08:46:02 -0700

    Add comments in abstract wide column accessor

--- src/com/aquarin/dataabstraction/widecolumn/WideColumnStoreAccessor.java
+++ src/com/aquarin/dataabstraction/widecolumn/WideColumnStoreAccessor.java
@@ -3,6 +3,7 @@ package com.aquarin.dataabstraction.widecolumn;
 import java.io.IOException;
 
 public interface WideColumnStoreAccessor {
+  // Should table name be given on each access call, or as part of the constructor?
   void InsertDataRow(String tableName, DataRow row) throws IOException; // What's ideal return value here?
   String InsertDataRowAutoKey(String tableName, DataRow row) throws IOException;
   
