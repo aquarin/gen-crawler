06aaf7f38e388aa3811c68355c99e24b45b64383
Ubuntu <user@master.(none)>
2014-05-01 23:13:48 -0700

    Remove tailing space

--- src/com/aquarin/dataabstraction/widecolumn/WideColumnStoreAccessor.java
+++ src/com/aquarin/dataabstraction/widecolumn/WideColumnStoreAccessor.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public interface WideColumnStoreAccessor {
   void InsertDataRow(DataRow row) throws IOException; // What's ideal return value here?
-  String InsertDataRowAutoKey(DataRow row) throws IOException;  
+  String InsertDataRowAutoKey(DataRow row) throws IOException;
   
   // Query definitions to come...
 }
