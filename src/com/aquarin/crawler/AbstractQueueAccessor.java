ebe1f789cbfc763510478a5b989cb96b8039f00b
Shuishiy ThinkPad <shuishi.yang@gmail.com>
2014-05-03 13:26:11 -0700

    Abstract Queue Accessor.

--- src/com/aquarin/crawler/AbstractQueueAccessor.java
+++ src/com/aquarin/crawler/AbstractQueueAccessor.java
@@ -1,6 +1,8 @@
 package com.aquarin.crawler;
 
+import java.io.IOException;
+
 public abstract class AbstractQueueAccessor {
   public abstract AbstractTaskQueueItem dequeue();
-  public abstract void enqueue(AbstractTaskQueueItem task);
+  public abstract void enqueue(AbstractTaskQueueItem task) throws IOException;
 }
