ce150309cf1c3493518adafc3af6741be774e251
Shuishiy ThinkPad <shuishi.yang@gmail.com>
2014-05-03 13:14:10 -0700

    Propagate the change of DataRow rename.

--- src/com/aquarin/crawler/AbstractTaskQueueItem.java
+++ src/com/aquarin/crawler/AbstractTaskQueueItem.java
@@ -1,10 +1,8 @@
 package com.aquarin.crawler;
 
-import com.aquarin.dataabstraction.widecolumn.DataRow;
+import com.aquarin.dataabstraction.DataRow;
 
 // TODO: think about if should separate the physical representation (DataRow) from this.
 public abstract class AbstractTaskQueueItem {
-  // TODO: think about what if it is not NoSQL/widecolumn storage.
-  // TODO: DataRow is a wide-column concept, this should not be associated with wide-column only.
   public abstract DataRow createDataRow();
 }
