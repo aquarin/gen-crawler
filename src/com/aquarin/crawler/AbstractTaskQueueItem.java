39391bf93903b3556d6afdb7921aac3e07d80c50
Shuishiy ThinkPad <shuishi.yang@gmail.com>
2014-05-03 13:08:58 -0700

    Add comment.

--- src/com/aquarin/crawler/AbstractTaskQueueItem.java
+++ src/com/aquarin/crawler/AbstractTaskQueueItem.java
@@ -5,5 +5,6 @@ import com.aquarin.dataabstraction.widecolumn.DataRow;
 // TODO: think about if should separate the physical representation (DataRow) from this.
 public abstract class AbstractTaskQueueItem {
   // TODO: think about what if it is not NoSQL/widecolumn storage.
+  // TODO: DataRow is a wide-column concept, this should not be associated with wide-column only.
   public abstract DataRow createDataRow();
 }
