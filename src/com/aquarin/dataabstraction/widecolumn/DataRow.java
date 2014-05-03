4513cb1974a54cf56a4bb7b239500f0f9c6f9d93
Shuishiy ThinkPad <shuishi.yang@gmail.com>
2014-05-03 11:59:51 -0700

    Add map initialization in DataRow

--- src/com/aquarin/dataabstraction/widecolumn/DataRow.java
+++ src/com/aquarin/dataabstraction/widecolumn/DataRow.java
@@ -1,9 +1,14 @@
 package com.aquarin.dataabstraction.widecolumn;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class DataRow {
   public String key;
   public Map<String, Object> properties; // Technically value type shouldn't 
                                          // be object. Ignore for now. 
+  
+  public DataRow() {
+    properties = new HashMap<String, Object>();
+  }
 }
