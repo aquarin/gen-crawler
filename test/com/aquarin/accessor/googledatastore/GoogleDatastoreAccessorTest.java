9615085315270d6639001b5e0ac5a058a8929d01
ThinkPad Ubuntu <shuishi.yang@gmail.com>
2014-05-03 10:02:40 -0700

    Test a commit

--- test/com/aquarin/accessor/googledatastore/GoogleDatastoreAccessorTest.java
+++ test/com/aquarin/accessor/googledatastore/GoogleDatastoreAccessorTest.java
@@ -1,5 +1,17 @@
 package com.aquarin.accessor.googledatastore;
 
-public class GoogleDatastoreAccessorTest {
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+@RunWith(JUnit4.class)
+public class GoogleDatastoreAccessorTest {
+  @Test
+  public void testInsertEntity() {
+    
+  }
+  
+  public static void main(String[] args) {
+    System.out.println("hello");
+  }
 }
