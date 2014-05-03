5b279ea73626f18bf98e49b8dda1eb4387ec621f
Shuishiy ThinkPad <shuishi.yang@gmail.com>
2014-05-03 12:02:37 -0700

    Change unit test

--- test/com/aquarin/accessor/googledatastore/GoogleDatastoreAccessorTest.java
+++ test/com/aquarin/accessor/googledatastore/GoogleDatastoreAccessorTest.java
@@ -1,14 +1,35 @@
 package com.aquarin.accessor.googledatastore;
 
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Map;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import com.aquarin.dataabstraction.widecolumn.DataRow;
+
 @RunWith(JUnit4.class)
 public class GoogleDatastoreAccessorTest {
   @Test
-  public void testInsertEntity() {
-    
+  public void testInsertEntity() throws IllegalArgumentException, GeneralSecurityException, IOException {
+    GoogleDatastoreAccessor accessor = GoogleDatastoreAccessor
+        .createFromEnvSettings("shuishiy-dogfood");
+    DataRow datarow = new DataRow();
+    datarow.properties.put("testname", "GoogleDatastoreAccessorTest");
+    accessor.InsertDataRowAutoKey("test", datarow);
+  }
+  
+  // TODO: get rid of this after find better Google API without env var
+  @Test
+  public void testRequiredEnvVariables() {
+    Map<String, String> env = System.getenv();
+
+    assertTrue("DATASTORE_SERVICE_ACCOUNT must be set", env.get("DATASTORE_SERVICE_ACCOUNT") != null);
+    assertTrue("DATASTORE_PRIVATE_KEY_FILE must be set", env.get("DATASTORE_PRIVATE_KEY_FILE") != null);
   }
   
   public static void main(String[] args) {
